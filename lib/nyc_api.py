1cb74a9535f89e15240db4c62f6edfe7bb245983
Christian Garcia <bones192nd@gmail.com>
2023-08-18 11:53:41 -0400

    Lab

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -10,6 +10,25 @@ class GetPrograms:
     response = requests.get(URL)
     return response.content
 
+  def program_school(self):
 
-programs = GetPrograms().get_programs()
-print(programs)
+    programs = json.loads(self.get_programs())
+    # programs_list = []
+    # for program in programs:
+    #   programs_list.append(program["agency"])
+
+    programs_list = [program["agency"] for program in programs]
+
+    return programs_list
+
+
+# programs = GetPrograms().get_programs()
+# print(programs)
+
+# programs = GetPrograms()
+# programs_school = programs.program_school()
+
+programs_school = GetPrograms().program_school()
+
+for school in set(programs_school):
+  print(school)
